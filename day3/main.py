5e155ad4e18bc92c123711a936354424c28d110d
Preston <prescarlton@gmail.com>
2022-12-13 09:17:47 -0500

    day3 part2

--- day3/main.py
+++ day3/main.py
@@ -1,5 +1,5 @@
 def read_input(): 
-    with open('input.txt') as f:
+    with open('sample.txt') as f:
         return f.read().splitlines()
 
 def make_compartments():
@@ -8,10 +8,15 @@ def make_compartments():
 def part1():
     return sum([[(ord(x[0]) - 96 if ord(x[0]) > 96 else ord(x[0])-38) for x in set(y[0]) if x in set(y[1])][0] for y in make_compartments()])
 
-def main():
-  for line in make_compartments():
-    print(line)
+def make_elf_groups():
+  elves = read_input()
+  return [elves[i*3:((i*3)+3)]  for i,x in enumerate(elves) if(i*3 < len(elves))]
+
+def part2():
+  return sum([[(ord(x[0]) - 96 if ord(x[0]) > 96 else ord(x[0])-38) for x in set(y[0]) if x in set(y[1]) and x in set(y[2])][0] for y in make_elf_groups()])
+
+
 
 
 if __name__ == '__main__':
-  print(part1())
+  print(part2())
